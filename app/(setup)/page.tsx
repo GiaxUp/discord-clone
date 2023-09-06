9952c173cb6624596638a117aedadfedc07492be
Giacomo Della Peruta <giax.95@live.it>
2023-09-06 21:17:47 +0200

    initial modal UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -23,7 +24,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
