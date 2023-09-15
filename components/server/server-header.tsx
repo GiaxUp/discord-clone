bd64e6bf47b5601a245ffcf84f36ab2f48844a4f
Giacomo Della Peruta <giax.95@live.it>
2023-09-15 15:16:40 +0200

    added delete & leave modals

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -76,13 +76,17 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
             Delete server
             <TrashIcon className="h-4 2-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
             Leave server
             <LogOut className="h-4 2-4 ml-auto" />
           </DropdownMenuItem>
